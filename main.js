557a67a44edaa761993fbadfc0c44789a2d651af
Alessandro_Mondelli <alexs98@hotmail.it>
2020-03-04 17:25:49 +0100

    Ora si potranno aggiungere memo tramite una textbox

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var app = new Vue({
     el: "#app",
     data: {
-        empty : true,
+        empty : false,
         newMemo : false,
+        newMemoText : "",
+        memos : ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut "]
     },
     methods: {
-        hideShow() {
+        hideShow() { //Mostro o Nascondo elemento
             this.newMemo = !this.newMemo;
+        },
+
+        createMemo() { //creo memo
+            if (!this.newMemoText == "") { //Se non è stato inserito testo vuoto
+                this.memos.push(this.newMemoText); //Inserisco contenuto in array
+                this.newMemoText = ""; //Svuoto textbox
+                this.empty = false; //La sezione memo non è più vuota
+            }
         }
-
-
     }
 });
